f0b60aa7aff2f9722d7fc49b1b6a6a5f0b2690b1
Anshuman <anshuman8swain@gmail.com>
2023-01-25 16:56:27 +0530

    shifted to django

--- index.js
+++
@@ -1,6 +0,0 @@
-
-import './src';
-
-
-// npm i express bcryptjs jsonwebtoken consola esm mongoose yup nodemon dotenv
-
--- src/models/index.js
+++
@@ -1,7 +0,0 @@
-import Form from './form';
-import Patient from './patient';
-
-export default {
-    Form,
-    Patient
-};

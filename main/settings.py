34801fc3abb41472f3a09eee92a0a88db4f7b404
Rishav Kumar <rdrishav2002@yahoo.com>
2023-01-29 22:25:49 +0530

    Feat: Added models

--- main/settings.py
+++ main/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'bjdf8#g3)#=vnth3_jbee7ck3r)d810z1s=3611-20c#w(8p74##'
+
+
 print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
---
+++ stewardship/choice.py
@@ -0,0 +1,51 @@
+SPECIMEN_CHOICES = [("blood", "blood"),
+ ("urine", "urine"),
+("sputum", "sputum"),
+("CSF", "CSF"), 
+("Wound", "Wound"),
+("Pus", "Pus"),
+("Bal", "Bal"),
+("Stool", "Stool"),
+("Mini Bal", "Mini Bal"),
+("Aseptic Fluid", "Aseptic Fluid"),
+("Pleural Fluid", "Pleural Fluid"),
+("TISSUE", "TISSUE")]
+
+# Antibiotic Sensitivity Choices
+ANTIBIOTIC_CHOICES = [("Ampicillin", "Ampicillin"),
+    ("Amoxicillin/Clavulanic", "Amoxicillin/Clavulanic"),
+    ("Gentamicin", "Gentamicin"),
+    ("Co-trimoxazole", "Co-trimoxazole"),
+    ("Ciprofloxacin", "Ciprofloxacin"),
+    ("Cloxacillin", "Cloxacillin"),
+    ("Cefixime", "Cefixime"),
+    ("Tetracycline", "Tetracycline"),
+    ("Levofloxacin", "Levofloxacin"),
+    ("Doxycycline", "Doxycycline"),
+    ("Norfloxacin", "Norfloxacin"),
+    ("Penicillin", "Penicillin"),
+    ("Erythromycin", "Erythromycin"),
+    ("Linezolid", "Linezolid"),
+    ("Vancomycin", "Vancomycin"),
+    ("Imipenem", "Imipenem"),
+    ("Moxifloxacin", "Moxifloxacin"),
+    ("Ceftriaxone", "Ceftriaxone")]
+
+# Multi Drug Resistant Choices
+MDR_CHOICES = [("MDR", "MDR"),
+("Non MDR", "Non MDR"),
+("PS(Pan sensitive)", "PS(Pan sensitive)"),
+("NA(No organism)", "NA(No organism)"),
+]
+
+# Resistance Choices
+RESISTANCE_CHOICES = [("CRE( Carbapenemase resistant enterobactereciae)", "CRE( Carbapenemase resistant enterobactereciae)"),
+("ESBL(Extended spectrum beta lactamase)", "ESBL(Extended spectrum beta lactamase)"),
+("MRSA(Methicillin resistant staphylococcus aureus)", "MRSA(Methicillin resistant staphylococcus aureus)"),
+("VRE(Vancomycin resistant enterococcus)", "VRE(Vancomycin resistant enterococcus)"),
+("CRAB( Carbapenem resistant acinetobacter baumannii)", "CRAB( Carbapenem resistant acinetobacter baumannii)"),
+("Col Re( Colistin resistant)", "Col Re( Colistin resistant)"),
+("NA-None of the above", "NA-None of the above"),
+]
+
+SURITY_CHOICES = [("probable", "probable"), ("definite", "definite"),("NONE", "NONE")]

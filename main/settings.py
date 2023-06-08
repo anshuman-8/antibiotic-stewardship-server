132001365b6039603190bfa6a9fbf1dc5cc1d3db
Anshuman Swain <anshuman8swain@gmail.com>
2023-06-08 17:51:53 +0530

    db migrated to postgres

--- main/settings.py
+++ main/settings.py
@@ -72,8 +72,15 @@ WSGI_APPLICATION = "main.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'stewardship',
+        'USER': 'anshuman',
+        'PASSWORD': 'asdfghjk',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
